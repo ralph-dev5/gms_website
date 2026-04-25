dafca32753f92dc5ad1a519f24ebc8ce9b1a3410
ralph-dev5 <ralpadrianmarabillo05@gmail.com>
2026-04-25 12:10:04 +0800

    Fix mobile viewport and layout scaling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="scroll-smooth">
 <head>
     <meta charset="UTF-8">
-    <title>GMS</title>
+    {{-- CRITICAL: This line fixes the "zoomed out" look on your mobile phone --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>GMS - Garbage Management System</title>
 
+    {{-- Fonts & Scripts --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    
+    <style>
+        [x-cloak] { display: none !important; }
+    </style>
 </head>
-<body>
+<body class="antialiased bg-gray-50 text-gray-900 overflow-x-hidden">
 
-    {{ $slot }}
+    <main>
+        {{ $slot }}
+    </main>
 
 </body>
 </html>
