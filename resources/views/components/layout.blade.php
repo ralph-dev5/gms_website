6f8625ee41bd7f1ea49913664be59af57b68bd8e
ralph-dev5 <ralpadrianmarabillo05@gmail.com>
2026-04-30 19:37:09 +0800

    fix error

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,16 +2,14 @@
 <html lang="en" class="scroll-smooth">
 <head>
     <meta charset="UTF-8">
-    {{-- CRITICAL: This line fixes the "zoomed out" look on your mobile phone --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <title>GMS - Garbage Management System</title>
 
-    {{-- Fonts & Scripts --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    
+
     <style>
         [x-cloak] { display: none !important; }
     </style>
