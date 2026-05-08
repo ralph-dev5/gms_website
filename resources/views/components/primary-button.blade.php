e8974953fbf449a9e3d9968d3d9a338e1a2c8684
ralph-dev5 <ralpadrianmarabillo05@gmail.com>
2026-05-08 21:49:44 +0800

    add missing blade components for login page

---
+++ resources/views/components/primary-button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 transition']) }}>
+    {{ $slot }}
+</button>
